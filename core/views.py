0a588e4f3b048945c933a06ccf8d970bcd78a3fa
Furtadex <guilhermealexandrehernandez@yahoo.com.br>
2026-04-27 21:57:58 -0300

    Com carrinho e mais umas coisas

--- core/views.py
+++ core/views.py
@@ -23,3 +23,6 @@ def produto_detalhes(request,id):
     produto = Product.objects.get(id=id)
     productimage = ProductImage.objects.filter(product_id=id)
     return render(request, 'core/produto_detalhes.html', {'produto':produto, 'imagens':productimage})
+
+def carrinho(request):
+    return render(request, 'core/carrinho.html')
--- setup/urls.py
+++ setup/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import sobre, home, produtos, contato, produto_detalhes
+from core.views import sobre, home, produtos, contato, produto_detalhes, carrinho
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -27,5 +27,6 @@ urlpatterns = [
     path('produtos/', produtos, name='produtos'),
     path('contato/', contato, name='contato'),
     path('produto_detalhes/<str:id>', produto_detalhes, name='produto_detalhes'),
+    path('carrinho/', carrinho, name='carrinho'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
